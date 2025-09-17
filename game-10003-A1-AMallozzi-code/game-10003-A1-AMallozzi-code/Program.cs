5de4dd994a79343b1801dbbfe9b6b5f3b541367a
Stick <Stick@GEOFF>
2025-09-17 15:24:56 -0400

    Finished Code.

--- game-10003-A1-AMallozzi-code/game-10003-A1-AMallozzi-code/Program.cs
+++ game-10003-A1-AMallozzi-code/game-10003-A1-AMallozzi-code/Program.cs
@@ -11,12 +11,6 @@ string bonesaw = ("bonesaw");
 string hammer = ("hammer");
 string hands = ("hands");
 
-bool perfectOperation = false;
-bool goodOperation = false;
-bool okayOperation = false;
-bool badOperation = false;
-bool failedOperation = false;
-
 //start game
 
 Console.WriteLine("As the best doctor in North West Burmingshire");
@@ -24,14 +18,14 @@ Console.WriteLine("it is no wonder the duke himself has called upon you to perfo
 Console.WriteLine("The duke needs a Lung transplant, you must oblige.");
 Console.WriteLine("First, you will announce your last name!");
 //define user name
-    String userName = Console.ReadLine();
+String userName = Console.ReadLine();
 
 Console.WriteLine($"Yes yes of course you are THE Dr.{userName}!");
 Console.WriteLine("How old are you again? The years have not been kind to my memories.");
 
 //define user age and years in the medical field
-    int playerAge = int.Parse(Console.ReadLine());
-    int doctorAge = playerAge / 2;
+int playerAge = int.Parse(Console.ReadLine());
+int doctorAge = playerAge / 2;
 
 Console.WriteLine($"Ah yes of course you are {playerAge}, you've been studying medicine for {doctorAge} years!");
 
@@ -39,7 +33,6 @@ Console.WriteLine("Jolly good, The duke is on the operating table, and your tool
 Console.WriteLine("My scalpel!");
 Console.WriteLine("My circsaw!");
 Console.WriteLine("My bonesaw!");
-Console.WriteLine("My hammer!");
 Console.WriteLine("My hands!");
 Console.WriteLine("Remember good Doctor, a good choice of tool and sharp mind is needed to maintain the body condition.");
 Console.WriteLine("This is the Duke himself after all!");
@@ -52,8 +45,8 @@ if (skinTool.Contains(scalpel))
 {
     Console.WriteLine($"excellent choice Doctor {userName}, a true surgeons tool!");
     Console.WriteLine($"you draw a good strong slice, but what shape was the cut supposed to be in again? Was it an x or y?");
-    
-        string skinKnow = Console.ReadLine();
+
+    string skinKnow = Console.ReadLine();
     if (skinKnow.Contains("y"))
     {
         Console.WriteLine("Yes of course! The cut should be in the shape of a y.");
@@ -68,7 +61,7 @@ if (skinTool.Contains(scalpel))
         Console.WriteLine("Well the medical field is full of mysteries, best just try something...");
         bodyCondition -= 10;
     }
-        Console.WriteLine("you've opened the torso and revealed the ribs.");
+    Console.WriteLine("you've opened the torso and revealed the ribs.");
 }
 
 //circsaw choice
@@ -86,12 +79,12 @@ if (skinTool.Contains(circsaw))
     else if (skinKnow.Contains("x"))
     {
         Console.WriteLine("I'm not so sure that's right but you're the doctor!");
-        bodyCondition -= 15;
+        bodyCondition -= 20;
     }
     else
     {
         Console.WriteLine("Well the medical field is full of mysteries, best just try something...");
-        bodyCondition -= 15;
+        bodyCondition -= 20;
     }
     Console.WriteLine("You bring the violent blade across the chest, blood paints your scrubs and ruins the decor.");
     Console.WriteLine("Not perfect but you have revealed the ribs.");
@@ -103,33 +96,6 @@ if (skinTool.Contains(bonesaw))
     Console.WriteLine($"uhh... Doctor I am not too sure if that is a good idea...");
     Console.WriteLine("You begin the grisly process of flaying the skin in a motion like that of woodworking.");
     Console.WriteLine("But wait! What shape was the cut supposed to be in again? Was it an x or y?");
-
-    string skinKnow = Console.ReadLine();
-    if (skinKnow.Contains("y"))
-    {
-        Console.WriteLine("Yes of course! The cut should be in the shape of a y.");
-        bodyCondition -= 10;
-    }
-    else if (skinKnow.Contains("x"))
-    {
-        Console.WriteLine("I'm not so sure that's right but you're the doctor!");
-        bodyCondition -= 15;
-    }
-    else
-    {
-        Console.WriteLine("Well the medical field is full of mysteries, best just try something...");
-        bodyCondition -= 15;
-    }
-    Console.WriteLine("It's a symphony of flesh! what a mess...");
-    Console.WriteLine("Good Lord that'll leave a scar, but you are on to the ribs regardless.");
-}
-
-//hammer choice
-if (skinTool.Contains(hammer))
-{
-    Console.WriteLine("Doctor no! How did that even get into the operating room!?");
-    Console.WriteLine("You use the back of the hammer like a wrench, pulling and tearing the skin away!");
-    Console.WriteLine("Hold on! What shape was the cut supposed to be in again? Was it an x or y?");
 
     string skinKnow = Console.ReadLine();
     if (skinKnow.Contains("y"))
@@ -147,17 +113,16 @@ if (skinTool.Contains(hammer))
         Console.WriteLine("Well the medical field is full of mysteries, best just try something...");
         bodyCondition -= 30;
     }
-    Console.WriteLine("You have shown your twisted mind upon this day...");
-    Console.WriteLine("But you have revealed the ribs.");
+    Console.WriteLine("It's a symphony of flesh! what a mess...");
+    Console.WriteLine("Good Lord that'll leave a scar, but you are on to the ribs regardless.");
 }
-
 //hands choice
 if (skinTool.Contains(hands))
 {
     Console.WriteLine("Dear God, what on earth are you doing!?");
     Console.WriteLine("You crack your knuckles and get to grabbing and pulling the torso apart.");
     Console.WriteLine("You have violated the medical process.");
-    bodyCondition -= 30;
+    bodyCondition -= 40;
     Console.WriteLine("Yes, the ribs are revealed, but... at what cost?");
 }
 
@@ -168,7 +133,6 @@ Console.WriteLine("With the skin open, the ribs are next, perhaps a change of to
 Console.WriteLine("My scalpel!");
 Console.WriteLine("My circsaw!");
 Console.WriteLine("My bonesaw!");
-Console.WriteLine("My hammer!");
 Console.WriteLine("My hands!");
 Console.WriteLine("What shall you use?");
 
@@ -180,12 +144,12 @@ if (ribTool.Contains("scalpel"))
     Console.WriteLine("Doctor I admire your commitment to precision Doctor but...");
     Console.WriteLine("I don't know about this.");
     Console.WriteLine("Before you start, how many ribs do humans have again?");
-    
+
     int ribKnow = int.Parse(Console.ReadLine());
     if (ribKnow == 24)
     {
         Console.WriteLine($"Ah yes that is correct, {ribKnow}");
-        bodyCondition -= 10;
+        bodyCondition -= 5;
     }
     else
     {
@@ -207,7 +171,7 @@ if (ribTool.Contains("circsaw"))
     if (ribKnow == 24)
     {
         Console.WriteLine($"Ah yes that is correct, {ribKnow}");
-        bodyCondition -= 0;
+        bodyCondition -= 5;
     }
     else
     {
@@ -239,29 +203,6 @@ if (ribTool.Contains("bonesaw"))
     Console.WriteLine("A magnificent job, good sir.");
     Console.WriteLine("The ribs have been removed.");
 }
-
-//hammer choice
-if (ribTool.Contains("hammer"))
-{
-    Console.WriteLine("A bit unconventional and messy, but a tough job requires a tough tool I suppose.");
-    Console.WriteLine("Before you start, how many ribs do humans have again?");
-
-    int ribKnow = int.Parse(Console.ReadLine());
-    if (ribKnow == 24)
-    {
-        Console.WriteLine($"Ah yes that is correct, {ribKnow}");
-        bodyCondition -= 5;
-    }
-    else
-    {
-        Console.WriteLine("Numbers were never your strong suit...");
-        bodyCondition -= 15;
-    }
-    Console.WriteLine("You raise the hammer high and bring it down harsh on the sternum.");
-    Console.WriteLine("a series of consecutive hits loosens the cage enough for it to be pulled.");
-    Console.WriteLine("The ribs have been removed.");
-}
-
 //hands choice
 if (ribTool.Contains("hands"))
 {
@@ -272,12 +213,12 @@ if (ribTool.Contains("hands"))
     if (ribKnow == 24)
     {
         Console.WriteLine($"Ah yes that is correct, {ribKnow}");
-        bodyCondition -= 10;
+        bodyCondition -= 20;
     }
     else
     {
         Console.WriteLine("Numbers were never your strong suit...");
-        bodyCondition -= 10;
+        bodyCondition -= 30;
     }
     Console.WriteLine("You pull up your sleeves and wrap your fist around the first rib");
     Console.WriteLine("With a hard twist and yank, the rib is free with a disturbing pop.");
@@ -287,122 +228,11 @@ if (ribTool.Contains("hands"))
 //operating on the ribs complete
 //begin operating on the lungs
 
-Console.WriteLine("Now that the ribs are removed, the lungs are next, perhaps a change of tool is in order?");
-Console.WriteLine("Remember Doctor, the lungs must be removed at the trachea, a chamber of cartilage and bone.");
+Console.WriteLine("Now that the ribs are removed, the heart is next, perhaps a change of tool is in order?");
+Console.WriteLine("Remember Doctor, the heart has tough arteries, they will not be easy to seperate!");
 Console.WriteLine("My scalpel!");
 Console.WriteLine("My circsaw!");
 Console.WriteLine("My bonesaw!");
-Console.WriteLine("My hammer!");
-Console.WriteLine("My hands!");
-Console.WriteLine("What shall you use?");
-
-string lungtool = Console.ReadLine();
-
-//scalpel
-if (lungtool.Contains("scalpel"))
-{
-    Console.WriteLine("Interesting choice Doctor, you may have some ");
-    Console.WriteLine("");
-
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-        {
-        Console.WriteLine();
-        bodyCondition -= 0;
-        }
-    Console.WriteLine("The Lungs have been removed.");
-}
-//circsaw
-if (lungtool.Contains("circsaw"))
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-    {
-        Console.WriteLine();
-        bodyCondition -= 0;
-    }
-    Console.WriteLine("The Lungs have been removed.");
-}
-//bonesaw
-if (lungtool.Contains("bonesaw"))
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-    {
-        Console.WriteLine();
-        bodyCondition -= 0;
-    }
-    Console.WriteLine("The Lungs have been removed.");
-}
-//hammer
-if (lungtool.Contains("hammer"))
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-    {
-        Console.WriteLine();
-        bodyCondition -= 0;
-    }
-    Console.WriteLine("The Lungs have been removed.");
-}
-//hands
-if (lungtool.Contains("hands"))
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-    {
-        Console.WriteLine();
-        bodyCondition -= 0;
-    }
-    Console.WriteLine("The Lungs have been removed.");
-}
-
-//Lung operation complete
-
-Console.WriteLine("");
-Console.WriteLine("Now that the lungs are removed, the heart is next, perhaps a change of tool is in order?");
-Console.WriteLine("Remember Doctor, the heart is very delicate, be sure to take good care.");
-Console.WriteLine("My scalpel!");
-Console.WriteLine("My circsaw!");
-Console.WriteLine("My bonesaw!");
-Console.WriteLine("My hammer!");
 Console.WriteLine("My hands!");
 Console.WriteLine("What shall you use?");
 
@@ -411,95 +241,112 @@ string heartTool = Console.ReadLine();
 //scalpel
 if (heartTool.Contains("scalpel"))
 {
-    Console.WriteLine("");
-    Console.WriteLine("");
+    Console.WriteLine("Interesting choice doctor, it won't be easy with such a thin");
+    Console.WriteLine("tool but things could be worse I suppose!");
+    Console.WriteLine("But just one minute! How many chambers does the heart have again?");
 
-    string heartKnow = Console.ReadLine();
-    if (heartKnow.Contains(""))
+    int heartKnow = int.Parse(Console.ReadLine());
+    if (heartKnow == 4)
     {
-        Console.WriteLine("");
-        bodyCondition -= 0;
+        Console.WriteLine("Bloody well done Doctor, that is correct!");
+        bodyCondition -= 10;
     }
     else
     {
-        Console.WriteLine();
-        bodyCondition -= 0;
+        Console.WriteLine("Caution to the wind I suppose, best just get on with it...");
+        bodyCondition -= 20;
     }
+    Console.WriteLine("You work up a sweat sawing at the tough tissue but manage to get through");
     Console.WriteLine("The heart has been removed.");
 }
 //circsaw
 if (heartTool.Contains("circsaw"))
 {
-    Console.WriteLine("");
-    Console.WriteLine("");
+    Console.WriteLine("Perfect choice doctor! The circsaw will go through arteries like butter!");
+    Console.WriteLine("But just one minute! How many chambers does the heart have again?");
 
-    string lungknow = Console.ReadLine();
-    if (lungknow.Contains(""))
+    int heartKnow = int.Parse(Console.ReadLine());
+    if (heartKnow == 4)
     {
-        Console.WriteLine("");
+        Console.WriteLine("Bloody well done Doctor, that is correct!");
         bodyCondition -= 0;
     }
     else
     {
-        Console.WriteLine();
-        bodyCondition -= 0;
+        Console.WriteLine("Caution to the wind I suppose, best just get on with it...");
+        bodyCondition -= 10;
     }
+    Console.WriteLine("With the gentle motion of a composer, you seperate the arteries from the heart.");
     Console.WriteLine("The heart has been removed.");
 }
 //bonesaw
 if (heartTool.Contains("bonesaw"))
 {
-    Console.WriteLine("");
-    Console.WriteLine("");
+    Console.WriteLine("Doctor I know I said the arteries were tough but that may be overkill...");
+    Console.WriteLine("But just one minute! How many chambers does the heart have again?");
 
-    string heartKnow = Console.ReadLine();
-    if (heartKnow.Contains(""))
+    int heartKnow = int.Parse(Console.ReadLine());
+    if (heartKnow == 4)
     {
-        Console.WriteLine("");
-        bodyCondition -= 0;
+        Console.WriteLine("Bloody well done Doctor, that is correct!");
+        bodyCondition -= 10;
     }
     else
     {
-        Console.WriteLine();
-        bodyCondition -= 0;
+        Console.WriteLine("Caution to the wind I suppose, best just get on with it...");
+        bodyCondition -= 20;
     }
-    Console.WriteLine("The heart has been removed.");
-}
-//hammer
-if (heartTool.Contains("hammer"))
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    string heartKnow = Console.ReadLine();
-    if (heartKnow.Contains(""))
-    {
-        Console.WriteLine("");
-        bodyCondition -= 0;
-    }
-    else
-    {
-        Console.WriteLine();
-        bodyCondition -= 0;
-    }
+    Console.WriteLine("You begin sawing, blood paints the walls and your gown as you");
+    Console.WriteLine("push the blade back and forth.");
     Console.WriteLine("The heart has been removed.");
 }
 //hands
 if (heartTool.Contains("hands"))
 {
-    Console.WriteLine("");
-    Console.WriteLine("");
+    Console.WriteLine("Dear Christ in Heaven! What demon has possessed you!");
+    Console.WriteLine("You're bloody hands! You daft maniac.");
+    Console.WriteLine("But just one minute! How many chambers does the heart have again?");
 
-    string heartKnow = Console.ReadLine();
-    if (heartKnow.Contains(""))
+    int heartKnow = int.Parse(Console.ReadLine());
+    if (heartKnow == 4)
     {
-        Console.WriteLine("");
-        bodyCondition -= 0;
+        Console.WriteLine("Bloody well done Doctor, that is correct!");
+        bodyCondition -= 30;
     }
     else
     {
-        Console.WriteLine();
-        bodyCondition -= 0;
+        Console.WriteLine("Caution to the wind I suppose, best just get on with it...");
+        bodyCondition -= 40;
     }
+    Console.WriteLine("With both hands you firmly grip the heart...");
+    Console.WriteLine("Yoou bring your heels to the edge of the table and assume a crouched position as you tug");
+    Console.WriteLine("with all of your weight.");
+    Console.WriteLine("A grotesque pop and you fly back onto your bottom, the bloody mass in your lap.");
     Console.WriteLine("The heart has been removed.");
+}
+
+bool surgeryZero = bodyCondition < 0;
+bool surgeryAbove = bodyCondition > 50;
+bool surgeryBelow = bodyCondition <= 50;
+bool surgeryFailure = surgeryBelow && surgeryZero;
+
+Console.WriteLine($"Well that was something Dr.{userName}.");
+
+if (surgeryAbove == true)
+{
+    Console.WriteLine("Well done, the Duke is saved and Burmingshire is in your gratitude!");
+}
+else if (surgeryAbove == false)
+{
+    if (surgeryBelow == true)
+    {
+        if (surgeryFailure == false)
+        {
+            Console.WriteLine("It would seem you've mauled the Duke, such a failure is astonding.");
+        }
+        else
+        {
+            Console.WriteLine("The poor Duke is unrecognizable, is this mass of blood and bone even human?");
+        }
+    }
 }
